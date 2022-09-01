2cbbc27dc2c60d0969515eb51f1f6b42029b092e
Андрей Мамаев <street.len@gmail.com>
2022-09-01 18:58:00 +0300

    change pagination

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -93,10 +93,12 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES':
+    'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-    'DEFAULT_AUTHENTICATION_CLASSES':
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
+    ],
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 6,
