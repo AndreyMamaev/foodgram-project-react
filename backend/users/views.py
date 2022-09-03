3dfc59bdbad4e711b20dd28cecfe99025de1709c
Андрей Мамаев <street.len@gmail.com>
2022-09-03 21:04:33 +0300

    add status to response subscribe

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404
 from djoser.views import UserViewSet
-from rest.framework import status
+from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
